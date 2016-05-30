5b890c6a1f974e3fa933d8c4cea5f32118bf5e57
Mithun Dhiman <mi2oon@gmail.com>
2016-05-30 22:41:42 +0200

    tests added

---
+++ test/property.js
@@ -0,0 +1,57 @@
+import test from 'ava';
+import stylelint from 'stylelint';
+import config from '../';
+
+/* eslint-disable */
+const validCss = (`
+.selector {
+    margin: 0;
+    margin-left: 20px;
+    background: #fff;
+}
+`)
+
+const invalidCss = (`
+.selector {
+    background:#FFFFFF;
+    margin-left: 20PX;
+    margin: 0;
+}
+`)
+/* eslint-enable */
+
+test('There are no warnings with properties CSS', t => {
+    return stylelint.lint({
+        code: validCss,
+        config: config
+    })
+    .then(data => {
+        const { errored, results } = data;
+        const { warnings } = results[0];
+
+        t.falsy(errored, 'no errored');
+        t.is(warnings.length, 0, 'flags no warnings');
+    });
+});
+
+
+test('There are warnings with invalid properties CSS', t => {
+    return stylelint.lint({
+        code: invalidCss,
+        config: config
+    })
+    .then(data => {
+        const { errored, results } = data;
+        const { warnings } = results[0];
+
+        t.truthy(errored, 'errored');
+        t.is(warnings.length, 7, 'flags seven warnings');
+        t.is(warnings[0].text, 'Expected "#FFFFFF" to be "#ffffff" (color-hex-case)', 'correct warning text');
+        t.is(warnings[1].text, 'Expected "#FFFFFF" to be "#FFF" (color-hex-length)', 'correct warning text');
+        t.is(warnings[2].text, 'Unexpected shorthand "margin" after "margin-left" (declaration-block-no-shorthand-property-overrides)', 'correct warning text');
+        t.is(warnings[3].text, 'Expected single space after ":" with a single-line value (declaration-colon-space-after)', 'correct warning text');
+        t.is(warnings[4].text, 'Expected property "margin-left" to come before property "background" (declaration-block-properties-order)', 'correct warning text');
+        t.is(warnings[5].text, 'Expected property "margin" to come before property "margin-left" (declaration-block-properties-order)', 'correct warning text');
+        t.is(warnings[6].text, 'Expected "PX" to be "px" (unit-case)', 'correct warning text');
+    });
+});
---
+++ test/values.js
@@ -0,0 +1,61 @@
+import test from 'ava';
+import stylelint from 'stylelint';
+import config from '../';
+
+/* eslint-disable */
+const validCss = (`
+.selector { /* Correct usage of quotes */
+    background-image: url('images/bg.png');
+    font-family: Helvetica Neue, sans-serif;
+}
+
+.another-selector { /* Correct usage of zero values */
+    font-family: Georgia, serif;
+    text-shadow:
+        0 -1px 0 rgba(0, 0, 0, .5),
+        0 1px 0 #fff;
+}
+`)
+
+const invalidCss = (`
+.class { /* Avoid missing space and semicolon */
+    background:#fff
+}
+
+.class { /* Avoid adding a unit on a zero value */
+    margin: 0px 0 20px 0;
+}
+`)
+/* eslint-enable */
+
+test('There are no warnings with values CSS', t => {
+    return stylelint.lint({
+        code: validCss,
+        config: config
+    })
+    .then(data => {
+        const { errored, results } = data;
+        const { warnings } = results[0];
+
+        t.falsy(errored, 'no errored');
+        t.is(warnings.length, 0, 'flags no warnings');
+    });
+});
+
+test('There are warnings with invalid values CSS', t => {
+    return stylelint.lint({
+        code: invalidCss,
+        config: config
+    })
+    .then(data => {
+        const { errored, results } = data;
+        const { warnings } = results[0];
+
+        t.truthy(errored, 'errored');
+        t.is(warnings.length, 4, 'flags eight warnings');
+        t.is(warnings[0].text, 'Expected a trailing semicolon (declaration-block-trailing-semicolon)', 'correct warning text');
+        t.is(warnings[1].text, 'Expected single space after ":" with a single-line value (declaration-colon-space-after)', 'correct warning text');
+        t.is(warnings[2].text, 'Unexpected duplicate selector ".class" (no-duplicate-selectors)', 'correct warning text');
+        t.is(warnings[3].text, 'Unexpected unit on zero length number (number-zero-length-no-unit)', 'correct warning text');
+    });
+});
