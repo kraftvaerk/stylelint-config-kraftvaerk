f8d4608e5801dae29d7031eaf0e7b496f51a20f4
Kristofor Salmin <racse1@users.noreply.github.com>
2018-06-28 18:36:32 +0300

    Revert Babel

--- __tests__/utils/getFlatLinterResult.js
+++ __tests__/utils/getFlatLinterResult.js
@@ -1,6 +1,8 @@
-export default function getFlatLinterResult(result) {
+'use strict';
+
+module.exports = function getFlatLinterResult(result) {
     const { errored, results } = result;
     const { warnings } = results[0];
 
     return { errored, warnings };
-}
+};
